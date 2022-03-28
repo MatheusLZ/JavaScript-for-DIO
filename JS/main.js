5a60b2bb91d2589a4abf1cf891786b7065656e2e
MatheusL <80010709+MatheusLZ@users.noreply.github.com>
2022-03-28 17:19:33 -0300

    Console2

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,9 @@
 var nome = "Matheus";
 var idade = 23;
 var idade2 = 10;
+var frase = "pelo amor de deus";
 //alert(nome + "tem " + idade + " anos.");
 //alert(idade + idade2);
 console.log(nome);
 console.log(idade);
+console.log(frase);
