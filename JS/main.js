351cb9cc57b2e0ad4d39e1fdcec412acf0a6c5fd
MatheusL <80010709+MatheusLZ@users.noreply.github.com>
2022-04-04 19:42:56 -0300

    Estrutura de repetição while

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,5 @@
-var idade = prompt("Qual é a sua idade?")
-
-if (idade >= 18){
-    alert("maior de idade, você pode passar")
+var count = 0
+while (count <= 5){
+    console.log(count)
+    count = count +1
 }
-else {
-    alert("menor de idade, VOCÊ...NÃO VAI...PASSAR!")
-}
-
-
-/*var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
-console.log(frutas)
-alert(frutas[1].nome)*/
-
-//lista.pop();
-//console.log(lista)
-//console.log(lista.toString());
-//console.log(lista.join(" - "))
-/*var nome = "Matheus";
-var idade = 23;
-var idade2 = 10;
-var frase = "pelo amor de deus";
-//alert(nome + "tem " + idade + " anos.");
-//alert(idade + idade2);
-console.log(nome);
-console.log(idade);
-console.log(frase.replace("deus", "Theus").toLowerCase());*/
