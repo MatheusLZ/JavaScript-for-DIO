cf0951373ace74f77ba105ddff4e4f423d657056
MatheusL <80010709+MatheusLZ@users.noreply.github.com>
2022-04-04 18:35:16 -0300

    Array

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
+var lista = ["maça", "banana", "mamão"]
+console.log(lista[1])
 
-var nome = "Matheus";
+/*var nome = "Matheus";
 var idade = 23;
 var idade2 = 10;
 var frase = "pelo amor de deus";
@@ -7,4 +9,4 @@ var frase = "pelo amor de deus";
 //alert(idade + idade2);
 console.log(nome);
 console.log(idade);
-console.log(frase.replace("deus", "Theus").toLowerCase());
+console.log(frase.replace("deus", "Theus").toLowerCase());*/
