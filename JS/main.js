1a3862b8863fb1770a27271c5219b58619aafb7f
MatheusL <80010709+MatheusLZ@users.noreply.github.com>
2022-05-05 20:19:04 -0300

    atualização depois de anos a fim

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,12 @@
-var d = new Date();
-alert(d.getFullYear())
-alert(d.getMonth())
-alert(d.getDay());
-alert(d.getHours())
-alert(d.getMinutes())
-alert(d.getSeconds())
+function validaIdade (idade){
+    var validar;
+    if (idade >= 18){
+    validar = true
+}
+    else{
+    validar = false
+}
+}
+
+var idade = prompt("Qual sua idade? ");
+console.log(validaIdade(idade));
